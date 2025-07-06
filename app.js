4db1d03d01d28fbcf1e19f6f07dc1ff058e2bd7a
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-06 16:58:12 +0530

    remove code to create sample documents in yelpcamp/campgrounds collection

--- app.js
+++ app.js
@@ -19,18 +19,6 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground",async (req, res) => {
-    const camp = new Campground({
-        title: "My Backyard",
-        price: "20",
-        description: "Cheap camping in my backyard",
-        location: "My House",
-    })
-    await camp.save();
-    res.send(camp);
-});
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
