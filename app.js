cc9437cd4a19c91626263dc9f69d5e629b1faab7
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-06 17:13:37 +0530

    add index.ejs page to list the campground titles

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-
 app.get("/", (req, res) => {
   res.render("home");
 });
-
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", {campgrounds});
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
