1bced002f222a6447cb99dcbc0c4adc21f950a3b
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-06 19:06:10 +0530

    add new.ejs and show.ejs and modify app.js and index.ejs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Campground = require("./models/campground");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 const db = mongoose.connection;
@@ -17,9 +18,25 @@ db.once("open", () => {
 app.get("/", (req, res) => {
   res.render("home");
 });
+
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", {campgrounds});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
