0ab16f89c9e110698f5e762253640da9816e5011
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-07 18:38:55 +0530

    add basic bootstrap styling

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
+const { places, descriptors, description_base } = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 const db = mongoose.connection;
@@ -18,10 +18,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 20;
     const camp = new Campground({
       title: `${randomInArray(descriptors)} ${randomInArray(places)}`,
+      image: `https://picsum.photos/400?random=${Math.floor(
+        Math.random() * 1000000000000
+      )}`,
+      price: price,
+      description: `${randomInArray(description_base)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      price: `$ ${random1000}`,
     });
     await camp.save();
   }
